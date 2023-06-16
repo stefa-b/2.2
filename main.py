b741a9b461047b1a4f3243ee43b275ec33d0f8bd
Stefania <126661019+stefa-b@users.noreply.github.com>
2023-06-16 12:46:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-import math
-import sys
-# Постоянная Эйлера.
-EULER = 0.5772156649015328606
-# Точность вычислений.
-EPS = 1e-10
-
-if __name__ == '__main__':
-    x = float(input("Value of x? "))
-    if x == 0:
-        print("Illegal value of x", file=sys.stderr)
-        exit(1)
-    a = x
-    S, k = a, 1
-# Найти сумму членов ряда.
-    while math.fabs(a) > EPS:
-        a *= x * k / (k + 1) ** 2
-        S += a
-        k += 1
-# Вывести значение функции.
-    print(f"Ei({x}) = {EULER + math.log(math.fabs(x)) + S}")
+a = 0
+j = 0
+for i in range (2):
+    a = a + 3
+    j += 2
+print (a,'час =',j ,'клеток')
+print (end =' ')
